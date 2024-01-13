e2c3ff941be08fe38e8c7a1dbd430f3046b47a52
moosius1 <imoe61@gmail.com>
2024-01-12 22:16:47 -0700

    Final commit w01 redone

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 // express web server
-import(dotenv);
-const express = require('express');
+
 const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
 const app = express();
 
-dotenv.config();
-console.log(dotenv.env.DB_USERNAME);
+console.log(process.env.DB_USERNAME);
 
-const port = 3000;
- 
+const port = process.env.PORT || 3000;
+
 app.use('/', require('./routes'));
 
-app.listen(process.env.port || 3000);
-console.log('Web Server is listending at port ' + (process.env.port || 3000));
+app.listen(port, () => {
+  console.log('Web Server is listening at port ' + port);
+});
