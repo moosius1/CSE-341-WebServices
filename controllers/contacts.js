688893f3be5c3ce84d29f346fbf9d362e9849be1
moosius1 <imoe61@gmail.com>
2024-01-27 00:53:43 -0700

    Final Commit w03

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const mongodb = require('../mongoConnection');
 const bodyParser = require('body-parser');
 const ObjectId = require('mongodb').ObjectId;
@@ -34,4 +35,66 @@ const eddyRoute = (req, res) =>{
 };
 
 
-module.exports = {getAll, getSingle,eddyRoute };
+
+const createNew = async(req,res) =>{
+    const contact= {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().db("test-contacts").collection('contacts').insertOne(contact);
+    if (response.acknowledged) {
+        res.status(201).json(response);
+    }
+    else{
+        res.status(500).json(response.error || 'Error while creating contact');
+    }
+};
+
+const updateExisting = async (req,res) =>{
+    const userId = new ObjectId(req.params.id);
+    //taken from create new same structure
+    const contact= {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+
+    const result = await mongodb
+    .getDb()
+    .db("test-contacts")
+    .collection('contacts')
+    .replaceOne({ _id: userId}, contact);
+
+    //used to log response if successfully replaced
+    //I'd be interested to know if we can push the results to a response like how we would with our GET requests. 
+    //Maybe this isn't possible due to the difference in PUT vs GET. 
+    console.log(result);
+    if (result.modifiedCount > 0){
+        res.status(204).send(result);
+    } else {
+        res.status(500).json(result.error || 'Error while updating contact')
+    }
+};
+
+const deleteEntry = async(req,res) =>{
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+    .getDb()
+    .db("test-contacts")
+    .collection('contacts')
+    .deleteOne({ _id: userId}, true);
+    if(result.deletedCount > 0) {
+        res.status(204).send(result);
+    } else {
+        res.status(500).json(result.error || 'Error Contact previously deleted or does not exist') 
+    }
+};
+
+
+
+module.exports = {getAll, getSingle,eddyRoute,createNew,updateExisting,deleteEntry };
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
-
+//Week 2 routes
 router.get('/', contactsController.getAll);
-
 router.get('/:id', contactsController.getSingle);
 
-router.get('eddy',contactsController.eddyRoute);
+//Week 3 routs
+router.post('/', contactsController.createNew);
+router.put('/:id', contactsController.updateExisting);
+router.delete('/:id', contactsController.deleteEntry);
+
 
 module.exports = router;
