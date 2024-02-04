aa7f3636b45ccec19210e300a4917c51e176d855
moosius1 <imoe61@gmail.com>
2024-02-03 19:31:41 -0700

    correcting host URL in swagger.json

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Temple API',
   },
-  host: 'https://cse-341-webservices.onrender.com/',
-  schemes: ['http'],
+  host: 'cse-341-webservices.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
